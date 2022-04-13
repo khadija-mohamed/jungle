bcd41ffe06e2459f592bbca6c53da4288fd10253
Khadija Mohamed <khadijam@live.ca>
2022-04-13 02:58:43 -0400

    admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @productCount = Product.all
+    @categoryCount = Category.all
   end
 end
